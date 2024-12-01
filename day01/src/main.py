0bd57cc30be54644c05f923f0e2906d4c9a4583e
Kirill Arkhipenko <aks1983@mail.ru>
2024-12-01 23:24:23 +0100

    day 01

---
+++ day01/src/main.py
@@ -0,0 +1,27 @@
+from collections import Counter
+from pathlib import Path
+
+
+def parse_input():
+    data = Path('my_input.txt').read_text().splitlines()
+    return zip(*[map(int, line.split()) for line in data])
+
+
+def part1():
+    left_list, right_list = map(sorted, parse_input())
+    total_distance = sum(abs(l - r) for l, r in zip(left_list, right_list))
+
+    print(f'Answer: {total_distance}')
+
+
+def part2():
+    left_list, right_list = parse_input()
+    right_count = Counter(right_list)
+    similarity_score = sum(l * right_count[l] for l in left_list)
+
+    print(f'Answer: {similarity_score}')
+
+
+if __name__ == '__main__':
+    part1()
+    part2()
---
+++ day01/src/main.rs
@@ -0,0 +1,51 @@
+use std::collections::HashMap;
+use std::fs;
+
+fn parse_input() -> (Vec<i32>, Vec<i32>) {
+    fs::read_to_string("my_input.txt")
+        .expect("Error reading file")
+        .lines()
+        .map(|line| {
+            line.split_whitespace()
+                .map(|n| n.parse().unwrap())
+                .collect::<Vec<i32>>()
+        })
+        .map(|nums| (nums[0], nums[1]))
+        .unzip()
+}
+
+fn part1() {
+    let total_distance: i32 = {
+        let (mut left_list, mut right_list) = parse_input();
+        left_list.sort();
+        right_list.sort();
+        left_list
+            .iter()
+            .zip(right_list.iter())
+            .map(|(l, r)| (l - r).abs())
+            .sum()
+    };
+
+    println!("Answer: {}", total_distance);
+}
+
+fn part2() {
+    let similarity_score: i32 = {
+        let (left_list, right_list) = parse_input();
+        let right_count = right_list.into_iter().fold(HashMap::new(), |mut acc, num| {
+            *acc.entry(num).or_insert(0) += 1;
+            acc
+        });
+        left_list
+            .iter()
+            .map(|num| num * right_count.get(num).unwrap_or(&0))
+            .sum()
+    };
+
+    println!("Answer: {}", similarity_score);
+}
+
+fn main() {
+    part1();
+    part2();
+}
